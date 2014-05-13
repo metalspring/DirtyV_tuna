de2cdeb0c873a89d4b0c45eb0cd97a28b3535dbd
metalspring <cgibson16137@gmail.com>
2014-05-13 19:02:00 -0400

    Remove dancedance since its broken

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -379,9 +379,6 @@ extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
-extern struct cpufreq_governor cpufreq_gov_dancedance;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_dancedance)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
 extern struct cpufreq_governor cpufreq_gov_abyssplug;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_abyssplug)
