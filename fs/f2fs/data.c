a012aa473676474abe3b4ee1eb524b345c4eb8e0
metalspring <cgibson16137@gmail.com>
2014-05-31 22:31:09 -0400

    Revert "f2fs: missing REQ_META and REQ_PRIO when sync_meta_pages(META_FLUSH)"
    This reverts commit 1fa4532ecae96305318a5e3321407a556ad1035f.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -144,7 +144,7 @@ void f2fs_submit_merged_bio(struct f2fs_sb_info *sbi,
 	/* change META to META_FLUSH in the checkpoint procedure */
 	if (type >= META_FLUSH) {
 		io->fio.type = META_FLUSH;
-		io->fio.rw = WRITE_FLUSH_FUA | REQ_META | REQ_PRIO;
+		io->fio.rw = WRITE_FLUSH_FUA;
 	}
 	__submit_merged_bio(io);
 	mutex_unlock(&io->io_mutex);
